88abb4dc7cb6e361ba174ed0e733b7d3c41da253
André Birk <arbi@itu.dk>
2025-03-21 01:13:52 +0100

    patch for deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  exportTrailingSlash: true,
+  output: 'export',
+  trailingSlash: true,
 };
 
 export default nextConfig;
